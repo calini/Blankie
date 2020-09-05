adaacfbf82695ac54386709ee057bc6232f349ab
Calin Ilie <calin@ilie.io>
2020-09-05 19:45:38 +0100

    Initial Commit

---
+++ Blankie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Blankie
+//
+//  Created by Calin Ilie on 05/09/2020.
+//  Copyright © 2020 Calin Ilie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
